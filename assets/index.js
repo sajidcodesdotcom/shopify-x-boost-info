60145c7f0876216b50858383cf953d61d65b8df2
sajidcodess <muhammadsajidkhan616@gmail.com>
2024-09-04 04:24:27 +0500

    added redirects

--- assets/index.js
+++ assets/index.js
@@ -13,8 +13,22 @@ const handleCopyToClipboard = () => {
 	});
 };
 
-const buildShopifyInfo = (info) => {
-	let html = `<h1>Shopify Info:</h1><ul>`;
+const handleRedirects = () => {
+	const links = document.querySelectorAll(".redirectLink");
+	links.forEach((link) => {
+		link.addEventListener("click", (e) => {
+			e.preventDefault();
+			const url = e.target.getAttribute("data-url");
+			console.log("clicked: ", url);
+			if (url) {
+				chrome.tabs.create({ url: url });
+			}
+		});
+	});
+};
+
+const buildInfoToCopyHTML = (info) => {
+	let html = "";
 	for (let i of info) {
 		const [title, value] = i.split("->");
 		html += `<li>
@@ -22,7 +36,32 @@ const buildShopifyInfo = (info) => {
             <input type="text" value="${value}" readonly>
             <button class="copy">Copy</button></li>`;
 	}
-	html += `<ul>`;
+
+	return html;
+};
+
+const buildInfoToRedirectHTML = (info) => {
+	let html = "";
+	for (let i of info) {
+		const [title, value] = i.split("->");
+		html += `<li>
+            <span class="title">${title}: &#128279;</span>
+            <a class="redirectLink" data-url=${value} href="#">${value}</a>
+            </li>`;
+	}
+
+	return html;
+};
+
+const buildHTML = (info) => {
+	const shopifyCopyInfo = info.shopify.copy;
+	const shopifyRedirectInfo = info.shopify.redirect;
+	const boostCopyInfo = info.boost.copy;
+	const boostRedirectInfo = info.boost.redirect;
+	let html = `<ul> <h1>Shopify Info:</h1>`;
+	html += buildInfoToCopyHTML(shopifyCopyInfo);
+	html += buildInfoToRedirectHTML(shopifyRedirectInfo);
+	html += `</ul>`;
 
 	return html;
 };
@@ -32,18 +71,48 @@ const constructPreviewLink = (location, id) => {
 		const currentURL = new URL(location.href);
 		currentURL.searchParams.set("preview_theme_id", id);
 		const updatedURL = currentURL.toString();
-		console.log(location, currentURL, updatedURL);
-
 		return updatedURL;
 	} catch (error) {
 		console.log(error);
 	}
 };
 
+const constructCollectionsAllLink = (location) => {
+	const currentURL = new URL(location.href);
+	const collectionsAll = currentURL.origin + "/collections/all";
+	return collectionsAll;
+};
+
+const prepareInfo = (data) => {
+	const shopifyInfo = JSON.parse(data.shopifyInfo);
+	const location = JSON.parse(data.location);
+
+	let info = {
+		shopify: { copy: [], redirect: [] },
+		boost: { copy: [], redirect: [] },
+	};
+	for (let i in shopifyInfo) {
+		if (i == "shop") {
+			info.shopify.copy.push(`Shop-> ${shopifyInfo[i]}`);
+		}
+		if (i == "theme") {
+			info.shopify.copy.push(`Theme ID-> ${shopifyInfo[i].id}`);
+			info.shopify.copy.push(`Theme Name-> ${shopifyInfo[i].name}`);
+			info.shopify.copy.push(
+				`Preview Link-> ${constructPreviewLink(location, shopifyInfo[i].id)}`,
+			);
+		}
+	}
+	info.shopify.redirect.push(
+		`Collections/all-> ${constructCollectionsAllLink(location)}`,
+	);
+	return info;
+};
+
 window.addEventListener("load", async () => {
 	const response = await chrome.storage.local.get("dataForPopup200");
 	const data = response.dataForPopup200;
-	console.log("got it in const data: ", JSON.Parse(data), !data.shopifyInfo);
+	console.log("got it in const data: ", data, !data.shopifyInfo);
 
 	const shopifyInfoElm = document.querySelector(".shopify-info");
 
@@ -52,25 +121,11 @@ window.addEventListener("load", async () => {
 		return;
 	}
 
-	const shopifyInfo = JSON.parse(data.shopifyInfo);
-	const location = JSON.parse(data.location);
+	const info = prepareInfo(data);
 
-	let info = [];
-	for (let i in shopifyInfo) {
-		if (i == "shop") {
-			info.push(`Shop-> ${shopifyInfo[i]}`);
-		}
-		if (i == "theme") {
-			info.push(`Theme ID-> ${shopifyInfo[i].id}`);
-			info.push(`Theme Name-> ${shopifyInfo[i].name}`);
-			info.push(
-				`Preview Link-> ${constructPreviewLink(location, shopifyInfo[i].id)}`,
-			);
-		}
-	}
-
-	shopifyInfoElm.innerHTML = buildShopifyInfo(info);
+	shopifyInfoElm.innerHTML = buildHTML(info);
 	handleCopyToClipboard();
+	handleRedirects();
 
 	console.log("data: ", data);
 });
--- assets/injected.js
+++ assets/injected.js
@@ -1,15 +1,21 @@
-(function() {
-  let boostObjects = {};
-  for (let key in window) {
-    if (key.includes("boost")) {
-      boostObjects[key] = window[key];
-    }
-  }
+(function () {
+	let boostObjects = {};
+	for (let key in window) {
+		if (key.includes("bcsf")) {
+			boostObjects.V1 = "V1";
+		} else if (key.includes("BoostPFS")) {
+			boostObjects.V2 = "V2";
+		} else if (key.includes("boostSDAppConfig") && key.includes("boostSD")) {
+			boostObjects.V3 = "V3";
+		} else if (key.includes("boostSD") && key.includes("boostSDData")) {
+			boostObjects.V3 = "Turbo";
+		}
+	}
 
-  console.log("running injected....");
+	console.log("running injected....");
 
-  const shopifyObject = JSON.stringify(window.Shopify);
-  boostObjects = JSON.stringify(boostObjects);
+	const shopifyObject = JSON.stringify(window.Shopify);
+	boostObjects = JSON.stringify(boostObjects);
 
-  window.postMessage({ type: "from_page", shopifyObject, boostObjects });
+	window.postMessage({ type: "from_page", shopifyObject, boostObjects });
 })();
