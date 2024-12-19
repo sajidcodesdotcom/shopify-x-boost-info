80cf1a3008df7270ca40a3594bae4f31d6a62931
sajidcodess <muhammadsajidkhan616@gmail.com>
2024-12-20 01:40:43 +0500

    feat: add code editor and customizer redirects

--- assets/content.js
+++ assets/content.js
@@ -1,34 +1,37 @@
 function injectScript(file) {
-  const script = document.createElement("script");
-  script.src = chrome.runtime.getURL(`assets/${file}`);
-  // script.onload = () => {
-  //   this.remove;
-  // };
-  //
-  (document.head || document.documentElement).appendChild(script);
+    const script = document.createElement("script");
+    script.src = chrome.runtime.getURL(`assets/${file}`);
+    // script.onload = () => {
+    //   this.remove;
+    // };
+    //
+    (document.head || document.documentElement).appendChild(script);
 }
 
 window.addEventListener("load", () => {
-  injectScript("injected.js");
+    injectScript("injected.js");
 });
 
 window.addEventListener("message", (event) => {
-  if (event.data.type && event.data.type === "from_page") {
-    const shopifyObject = event.data.shopifyObject
-      ? event.data.shopifyObject
-      : false;
-    const boostVersions = event.data.boostVersions;
-    const location = JSON.stringify(window.location);
-    const parsedShopifyObject = JSON.parse(shopifyObject);
+    if (event.data.type && event.data.type === "from_page") {
+        const shopifyObject = event.data.shopifyObject
+            ? event.data.shopifyObject
+            : false;
+        const boostVersions = event.data.boostVersions;
+        const location = JSON.stringify(window.location);
+        const parsedShopifyObject = JSON.parse(shopifyObject);
+        const widgetIntegrationGeneralSettings =
+            event.data.widgetIntegrationGeneralSettings;
 
-    if (parsedShopifyObject && parsedShopifyObject.shop) {
-      chrome.storage.local.set({
-        dataForPopup200: {
-          shopifyInfo: shopifyObject,
-          location: location,
-          boostVersions,
-        },
-      });
+        if (parsedShopifyObject && parsedShopifyObject.shop) {
+            chrome.storage.local.set({
+                dataForPopup200: {
+                    shopifyInfo: shopifyObject,
+                    location: location,
+                    boostVersions,
+                    widgetIntegrationGeneralSettings,
+                },
+            });
+        }
     }
-  }
 });
--- assets/injected.js
+++ assets/injected.js
@@ -1,26 +1,36 @@
 (function () {
-	if (window.Shopify) {
-		let boostVersions = {};
-		for (let key in window) {
-			if (key.includes("bcsf")) {
-				boostVersions.V1 = "V1";
-			}
-			if (key.includes("BoostPFS")) {
-				boostVersions.V2 = "V2";
-			}
-			if (key.includes("boostSDAppConfig") && key.includes("boostSD")) {
-				boostVersions.V3 = "V3";
-			}
-			if (
-				(key.includes("boostSD") && key.includes("boostSDData")) ||
-				key.includes("boostWidgetIntegration")
-			) {
-				boostVersions.V3 = "Turbo";
-			}
-		}
-
-		const shopifyObject = JSON.stringify(window.Shopify);
-		boostVersions = JSON.stringify(boostVersions);
-		window.postMessage({ type: "from_page", shopifyObject, boostVersions });
-	}
+    if (window.Shopify) {
+        let boostVersions = {};
+        let widgetIntegrationGeneralSettings = {};
+        for (let key in window) {
+            if (key.includes("bcsf")) {
+                boostVersions.V1 = "V1";
+            }
+            if (key.includes("BoostPFS")) {
+                boostVersions.V2 = "V2";
+            }
+            if (key.includes("boostSDAppConfig") && key.includes("boostSD")) {
+                boostVersions.V3 = "V3";
+            }
+            if (
+                key.includes("boostWidgetIntegration") &&
+                window.boostWidgetIntegration != "undefined"
+            ) {
+                boostVersions.turbo = "Turbo";
+                widgetIntegrationGeneralSettings =
+                    boostWidgetIntegration.generalSettings;
+            }
+        }
+        const shopifyObject = JSON.stringify(window.Shopify);
+        boostVersions = JSON.stringify(boostVersions);
+        widgetIntegrationGeneralSettings = JSON.stringify(
+            widgetIntegrationGeneralSettings
+        );
+        window.postMessage({
+            type: "from_page",
+            shopifyObject,
+            boostVersions,
+            widgetIntegrationGeneralSettings,
+        });
+    }
 })();
