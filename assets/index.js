b2e66d022a05ce57d03d9ea5dd5b35a080f4d089
sajidcodesdotcom <muhammadsajidkhan616@gmail.com>
2025-04-24 03:02:16 +0500

    fix bug (incorrect visual editor theme id)

--- assets/index.js
+++ assets/index.js
@@ -12,7 +12,7 @@ const handleCopyToClipboard = () => {
           <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
             <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
           </svg>
-          Copied!`;
+          `;
         button.classList.add("copied");
         // Reset button after 1 second
         setTimeout(() => {
@@ -53,7 +53,6 @@ const buildInfoToCopyHTML = (info) => {
                 <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
                 <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
               </svg>
-              Copy
             </button>
           </li>`;
   }
@@ -128,17 +127,17 @@ const constructURLLink = (location, id) => {
     const currentURL = new URL(location.href);
     const origin = currentURL.origin;
     const collectionsAll = origin + "/collections/all";
-    redirectLinks.collections_all = collectionsAll;
+    // redirectLinks.collections_all = collectionsAll;
     currentURL.searchParams.set("preview_theme_id", id);
     redirectLinks.preview_link = currentURL.toString();
     currentURL.search = "";
-    if (
-      currentURL.pathname.includes("/products") ||
-      currentURL.pathname.includes("/collections")
-    ) {
-      const jsonInfoLink = currentURL.toString() + ".json";
-      redirectLinks.current_page_JSON = jsonInfoLink;
-    }
+    // if (
+    //   currentURL.pathname.includes("/products") ||
+    //   currentURL.pathname.includes("/collections")
+    // ) {
+    //   const jsonInfoLink = currentURL.toString() + ".json";
+    //   redirectLinks.current_page_JSON = jsonInfoLink;
+    // }
     return redirectLinks;
   } catch (error) {
     console.log(error);
@@ -187,7 +186,7 @@ const prepareInfo = async (data) => {
   let shopWithoutDomain = shopifyInfo.shop.replace(".myshopify.com", "");
 
   if (Object.values(boostVersions).length != 0) {
-    info.boost.copy.push(`Boost Versions Detected->${versions}`);
+    info.boost.copy.push(`App Version->${versions}`);
     info.boost.redirect.push(
       `Shopify Integration->https://admin.shopify.com/store/${shopWithoutDomain}/apps/product-filter-search/shopify-integration`
     );
@@ -195,7 +194,7 @@ const prepareInfo = async (data) => {
 
   if (boostVersions.V3) {
     info.boost.redirect.push(
-      `Visual Editor->https://admin.shopify.com/store/${shopWithoutDomain}/apps/product-filter-search/theme-setting/176401613098/visual-editor`
+      `Visual Editor->https://admin.shopify.com/store/${shopWithoutDomain}/apps/product-filter-search/theme-setting/${shopifyInfo.theme.id}/visual-editor`
     );
   }
 
